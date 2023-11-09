977ac9ce65a5242b6f550fc3e0b5a8dba00237fa
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-11-09 11:39:42 +0530

    database connected

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,21 @@
+
+const mysql = require ('mysql2')
+
+const connections = mysql.createConnection({
+    // host:"127.0.0.1",
+    host:"localhost",
+    user:"root",
+    password:"Irshad@123",
+    database:"restaurant"
+})
+connections.connect((err)=>{
+    if(err){
+    console.error(`Error connecting to MySQL: ${err.message}`);
+    return;
+    } 
+    console.log("database created succesfully");
+})
+
 const Home=(req,res,next)=>{
     res.send("server working")
 }
